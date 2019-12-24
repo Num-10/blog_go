1c25fc4287446d68ee81840fa14a99f0dfd858b3
num_10 fc <num_10@163.com>
2019-12-24 17:21:25 +0800

    edit config name

--- conf/setting.go
+++ conf/setting.go
@@ -36,9 +36,9 @@ type Model struct {
 var ModelIni Model
 
 func Setup() {
-	config, err := ini.Load("conf/config.ini")
+	config, err := ini.Load("conf/app.ini")
 	if err != nil {
-		fmt.Println("load config.ini fail: " + err.Error())
+		fmt.Println("load app.ini fail: " + err.Error())
 		os.Exit(e.SERVICE_ERROR)
 	}
 	config_load := new(config_struct)
@@ -46,7 +46,7 @@ func Setup() {
 	AppIni = config_load.App
 	ModelIni = config_load.Model
 	if err != nil {
-		fmt.Println("load config.ini fail: " + err.Error())
+		fmt.Println("load app.ini fail: " + err.Error())
 		os.Exit(e.READ_CONFIG_ERROR)
 	}
 }
