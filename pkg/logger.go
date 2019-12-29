58f150335ee28119f35e73a12b73791aca0cea9f
num_10 pc <num_10@163.com>
2019-12-29 18:25:43 +0800

    log shook

--- pkg/logger.go
+++ pkg/logger.go
@@ -3,9 +3,12 @@ package pkg
 import (
 	"blog_go/conf"
 	"fmt"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
 	"os"
 	"path"
+	"time"
 )
 
 var Logger *logrus.Logger
@@ -26,7 +29,14 @@ func LogSetUp()  {
 		if os.IsNotExist(err) {
 			_, err = os.Stat(LogPath)
 			if os.IsNotExist(err) {
-
+				err = os.MkdirAll(LogPath, os.ModePerm)
+				if err != nil {
+					fmt.Println("create runtime/log fail：" + err.Error())
+				}
+			}
+			src, err = os.Create(fileName)
+			if err != nil {
+				fmt.Println("create runtime/log/service.log fail：" + err.Error())
 			}
 		} else {
 			fmt.Println("err", err)
@@ -40,6 +50,36 @@ func LogSetUp()  {
 
 	//设置日志级别
 	Logger.SetLevel(logrus.DebugLevel)
+
+	//设置 rotatelogs
+	logWriter, err := rotatelogs.New(
+		//分割后个文件名称
+		fileName + ".%Y%m%d.log",
+
+		//生成软链，指向最新日志文件
+		//rotatelogs.WithLinkName(fileName),
+
+		//设置最大保存时间（7天）
+		rotatelogs.WithMaxAge(7*24*time.Hour),
+
+		//设置日志切割时间间隔（1天）
+		rotatelogs.WithRotationTime(24*time.Hour),
+		)
+
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel: logWriter,
+		logrus.FatalLevel: logWriter,
+		logrus.DebugLevel: logWriter,
+		logrus.WarnLevel: logWriter,
+		logrus.ErrorLevel: logWriter,
+		logrus.PanicLevel: logWriter,
+	}
+
+	ifHook := lfshook.NewHook(writeMap, &logrus.TextFormatter{
+		TimestampFormat:"2006-01-02 15:04:05",
+	})
+
+	Logger.AddHook(ifHook)
 
 	//设置日志格式
 	Logger.SetFormatter(&logrus.TextFormatter{
