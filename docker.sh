280b9249e629ceab32b4e1cd44106a371d40472c
num_10 fc <num_10@163.com>
2020-01-10 16:06:24 +0800

    test

--- docker.sh
+++ docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd /home/lnmp/golang/blog_go
+#cd /home/lnmp/golang/blog_go
 echo '111'
 #生成镜像
 docker build -t blog_go .
 echo '222'
 #停止并删除旧容器
-docker stop blog_go
-docker rm blog_go
+#docker stop blog_go
+#docker rm blog_go
 #创建容器
-docker run --name blog_go -v /home/lnmp/golang/blog_go/runtime:/home/lnmp/golang/blog_go/runtime --network lnmp -p 8888:8888 -d blog_go
+#docker run --name blog_go -v /home/lnmp/golang/blog_go/runtime:/home/lnmp/golang/blog_go/runtime --network lnmp -p 8888:8888 -d blog_go
