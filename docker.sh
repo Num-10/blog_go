35847d2dd2d44b96d4c575c164dfe8afe502649a
num_10 fc <num_10@163.com>
2020-01-13 09:32:13 +0800

    test

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-cd /home/lnmp/golang/blog_go
-docker build -t blog_go .
+docker build -t blog_go -f /home/lnmp/golang/blog_go/Dockerfile .
 echo docker inspect blog_go | grep Id
 if docker inspect blog_go | grep Id
 then
   docker stop blog_go
   docker rm -f blog_go
   docker run --name blog_go -v /home/lnmp/golang/blog_go/runtime:/home/lnmp/golang/blog_go/runtime --network lnmp -p 8888:8888 -d blog_go
+fi
