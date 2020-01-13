e99719ee8e384c36bc1c11d540fff1126e0ec609
num_10 fc <num_10@163.com>
2020-01-13 10:19:55 +0800

    test

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 cd /home/lnmp/golang/blog_go
 docker build -t blog_go .
 
-if [ ! "$(docker ps -q -f name=blog_go)" ]; then
+if [ "$(docker ps -q -f name=blog_go)" ]; then
     if [ "$(docker ps -aq -f status=running -f name=blog_go)" ]; then
         # stop
         docker stop blog_go
@@ -11,6 +11,6 @@ if [ ! "$(docker ps -q -f name=blog_go)" ]; then
         # cleanup
         docker rm blog_go
     fi
-    # run container
-    docker run --name blog_go -v /home/lnmp/golang/blog_go/runtime:/home/lnmp/golang/blog_go/runtime --network lnmp -p 8888:8888 -d blog_go
 fi
+# run container
+docker run --name blog_go -v /home/lnmp/golang/blog_go/runtime:/home/lnmp/golang/blog_go/runtime --network lnmp -p 8888:8888 -d blog_go
