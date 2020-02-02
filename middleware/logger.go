3a0e0caca93f6ffd2f539506ccd4f3636fe5946d
num_10 pc <num_10@163.com>
2020-02-02 19:34:22 +0800

    end

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"blog_go/conf"
 	"blog_go/pkg"
 	"database/sql/driver"
 	"fmt"
@@ -39,7 +40,7 @@ func LoggerToFile() gin.HandlerFunc {
 		if len(c.Request.Header["Origin"]) > 0 {
 			method := c.Request.Method
 			// 核心处理方式
-			c.Header("Access-Control-Allow-Origin", c.Request.Header["Origin"][0])
+			c.Header("Access-Control-Allow-Origin", conf.AppIni.AllowRequestDomain)
 			c.Header("Access-Control-Allow-Credentials", "true")
 			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token")
 			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
