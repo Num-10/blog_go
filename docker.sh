7036977369f57dadc9a39e951e53cdbe101e1aaa
num_10 pc <num_10@163.com>
2020-02-24 12:54:18 +0800

    test docker contaiter

--- docker.sh
+++ docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-cd /home/lnmp/golang/blog_go
-docker build -t blog_go .
 
+# delete old images and container
 if [ "$(docker ps -q -f name=blog_go)" ]; then
     if [ "$(docker ps -aq -f status=running -f name=blog_go)" ]; then
         # stop
@@ -10,7 +9,12 @@ if [ "$(docker ps -q -f name=blog_go)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=blog_go)" ]; then
         # cleanup
         docker rm blog_go
+        docker rmi blog_go
     fi
 fi
+
+cd /home/lnmp/golang/blog_go
+docker build -t blog_go .
+
 # run container
 docker run --name blog_go -v /home/lnmp/golang/blog_go/runtime:/home/lnmp/golang/blog_go/runtime -v /etc/localtime:/etc/localtime:ro  --network lnmp -p 8888:8888 -d blog_go
